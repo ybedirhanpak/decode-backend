fecba95e903ae74c79cb5d7ed197ed5fa904acf4
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2025-01-04 13:26:59 +0300

    Increase response timeout to 5 minutes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import timeout from "connect-timeout";
 import { apiRoutes } from "./routes/api";
 
 dotenv.config();
@@ -12,6 +13,8 @@ const corsOptions = {
     origin: process.env.NODE_ENV === "production" ? process.env.APP_URL : "*",
 };
 
+// Set timeout to 5 minutes (300000 milliseconds)
+app.use(timeout("300s"));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", apiRoutes);
